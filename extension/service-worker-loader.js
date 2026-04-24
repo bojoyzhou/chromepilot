59ddd65a03c5416713283e14dd66f5838e20210f
刘荣良 <lrl02439331@alibaba-inc.com>
2026-04-24 16:53:14 +0800

    chore: update extension build output
    Rebuild extension assets after modular refactor.

--- extension/service-worker-loader.js
+++ extension/service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http://localhost:5174/@vite/env';
-import 'http://localhost:5174/@crx/client-worker';
-import 'http://localhost:5174/src/extension/background/index.ts';
+import './assets/index.ts-CO0aGh4Q.js';
